062b099f64ffe047010ee6ff402dd24312375cae
Alexander Santos <alexsantosantana@live.com>
2020-05-14 19:23:35 -0400

    changed the entry PORT

--- server.js
+++ server.js
@@ -206,6 +206,8 @@ app.post('/api/newTweet', (req, res) => {
 	});
 });
 
-app.listen(3001, () => {
-	console.log('Listening on \n  http://localhost:3001');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log('Listening on \n  http://localhost:' + PORT);
 });
